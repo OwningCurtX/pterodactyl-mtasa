163fc4751dfd600e2e6bd766b6599435f6d58c8d
Joseph <S2013190681@firstasia.edu.ph>
2017-10-08 20:50:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -e
 cd /home/container
 
-set -e
-exec ${STARTUP} -n -t -u
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+exec "${MODIFIED_STARTUP}" -n -t -u
