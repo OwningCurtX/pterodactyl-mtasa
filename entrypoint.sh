0bc8aa5b242be041cf54aa82c945aaaf1716f487
Joseph <S2013190681@firstasia.edu.ph>
2017-10-08 19:25:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,10 @@ echo ":/home/container$ ${MODIFIED_STARTUP}"
 # Fix for Rust not starting
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
 export NODE_DEBUG=fs
+chown -R container:container /home/container/
+chown -R container:container /home/container/latest.log
+chmod 777 -R /home/container/
+chmod 777 /home/container/latest.log
 
 # Run the Server
 node /wrapper.js "${MODIFIED_STARTUP}"
