d38d5d219434f72934370de74161ea7b16b014e6
Joseph <S2013190681@firstasia.edu.ph>
2017-10-08 19:38:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,5 @@ sleep 2
 
 cd /home/container
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
-# Fix for Rust not starting
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
-export NODE_DEBUG=fs
-chown -R container:container /home/container/
-chown -R container:container /home/container/latest.log
-chmod 777 -R /home/container/
-chmod 777 /home/container/latest.log
-
-# Run the Server
-node /wrapper.js "${MODIFIED_STARTUP}"
+set -e
+exec ${STARTUP} -n -t -u
