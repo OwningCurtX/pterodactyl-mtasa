fad00baf1eb92512a8e8bd99e252d08195db9e83
Joseph <S2013190681@firstasia.edu.ph>
2017-10-10 19:31:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ cd /home/container
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
-echo "GameNodePro Server is starting.. Please wait."
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
 exec ${MODIFIED_STARTUP} -n -t -u
